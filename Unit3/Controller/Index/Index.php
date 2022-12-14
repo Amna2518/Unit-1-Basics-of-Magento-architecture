1348870190d088d8682bf78a01e6df1046d43f18
Amna Hamid <amna.hamid@rltsquare.com>
2022-12-14 16:31:19 +0500

    unit3 module added

---
+++ Unit3/Controller/Index/Index.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RLTSquare\Unit3\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements ActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    protected PageFactory $_pageFactory;
+
+    /**
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        PageFactory $pageFactory
+    ) {
+        $this->_pageFactory = $pageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        return $this->_pageFactory->create();
+    }
+}
